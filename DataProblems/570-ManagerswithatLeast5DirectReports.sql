0806a605b16e5d21ecb2fa1a0451ec87888794b2
victor <vmalvaradop0@gmail.com>
2026-03-03 18:33:19 -0600

    Managers with at leaste 5 reports

--- DataProblems/570-ManagerswithatLeast5DirectReports.sql
+++ DataProblems/570-ManagerswithatLeast5DirectReports.sql
@@ -1,7 +1,11 @@
 -- Write your PostgreSQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT e2.name
-FROM employee e1
-LEFT JOIN employee e2
+FROM (
+    SELECT managerid
+    FROM employee
+    GROUP BY managerid
+    HAVING count(managerid) >= 5
+) e1
+JOIN employee e2
     ON e1.managerid = e2.id
-GROUP BY e1.managerid, e2.name
-HAVING count(*) >= 5
