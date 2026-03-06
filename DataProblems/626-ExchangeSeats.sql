340e0b6297f1b69690c7b4000bd68c96e9c0e443
victor <vmalvaradop0@gmail.com>
2026-03-05 19:16:49 -0600

    ExchangeSeats

---
+++ DataProblems/626-ExchangeSeats.sql
@@ -0,0 +1,9 @@
+SELECT
+    CASE
+        WHEN id % 2 = 1 AND id != (SELECT MAX(id) FROM seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM seat
+ORDER BY id
